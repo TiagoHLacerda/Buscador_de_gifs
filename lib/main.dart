018a2ace141eb7a370b63426df6bc846fb8f38c4
Tiago Lacerda <51682610+TiagoHLacerda@users.noreply.github.com>
2021-04-15 23:29:26 -0300

    Botao de compartilhar gif

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:buscador_de_gifs/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'ui/home_page.dart';
+
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home:HomePage(),
     theme: ThemeData(
         hintColor: Colors.white,
         inputDecorationTheme: InputDecorationTheme(
